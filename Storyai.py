dd7a57792c7c5df9eb639533d55a7526d3bf76e9
WeyzBusted <68425545+WeyzBusted@users.noreply.github.com>
2023-06-27 16:04:09 +0400

    Storyai.py

--- Storyai.py
+++ Storyai.py
@@ -1,68 +1,80 @@
-import logging
+import telegram
+from telegram.ext import Updater, CommandHandler
+import tensorflow as tf
+import numpy as np
 import requests
-import urllib
+from PIL import Image
+import io
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+# вставьте свой API-ключ
+telegram_bot_token = '6064933243:AAEgWPjI8DAhffvpKvIXVf8BDBzknzZ0sqI'
+bot = telegram.Bot(token=telegram_bot_token)
 
-# Настройка логгера
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+# загрузка нейросети pastel mix
+model_url = 'https://github.com/reiinakano/pastel/releases/download/v0.1.0/model_f16_1024.pb'
+model_file = requests.get(model_url).content
+sess = tf.compat.v1.Session()
+graph = tf.compat.v1.get_default_graph()
+graph_def = tf.compat.v1.GraphDef()
+graph_def.ParseFromString(model_file)
+sess.graph.as_default()
+tf.import_graph_def(graph_def)
 
-# Обработка команды /start
+# функция для генерации изображения
+def generate_image():
+    global graph
+    global sess
+
+    # загрузка изображения в формате numpy array
+    img = Image.open('input.jpg')
+    img_array = np.array(img)/255.
+
+    # ресайз изображения
+    img_resized = tf.image.resize_images(tf.convert_to_tensor(img_array), (256, 256))
+
+    # подготовка данных для нейросети
+    inputs = np.expand_dims(img_resized.eval(session=sess), axis=0)
+
+    # запуск нейросети
+    with sess.as_default():
+        with graph.as_default():
+            output_image = sess.run('generator/Tanh:0', {'input_image:0': inputs})
+
+    # сохранение сгенерированного изображения
+    output_image = output_image[0, :, :, :]
+    output_image = ((output_image + 1) / 2) * 255
+    output_image = output_image.astype(np.uint8)
+    Image.fromarray(output_image).save('output.jpg')
+
+# обработчик команды /start
 def start(update, context):
-    update.message.reply_text("Привет! Я могу помочь тебе сгенерировать картинку с помощью нейросети Pastel Mix. Просто отправь мне текст, и я сгенерирую для тебя картинку.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я могу сгенерировать картинку с помощью нейросети pastel mix. Напиши мне /generate, чтобы начать.")
 
-# Обработка сообщений с текстом
-def text_message(update, context):
-    text = update.message.text
-    image_url = generate_image(text)
+# добавление обработчика команды /start
+updater = Updater(token=telegram_bot_token, use_context=True)
+dispatcher = updater.dispatcher
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
-    # Отправляем картинку пользователю
-    update.message.reply_photo(photo=image_url)
+# обработчик команды /generate
+def generate(update, context):
+    # получение фото из последнего сообщения
+    message = update.message
+    file_id = message.photo[-1].file_id
+    newFile = bot.get_file(file_id)
+    newFile.download('input.jpg')
 
-# Функция для генерации изображения с помощью нейросети Pastel Mix
-def generate_image(text):
-    api_url = 'https://api.nnsplit.com/pastelmix'
+    # генерация нового изображения
+    generate_image()
 
-    # Создаем заголовок для запроса
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Mozilla/5.0'
-    }
+    # отправка сгенерированного изображения пользователю
+    with open('output.jpg', 'rb') as f:
+        context.bot.send_photo(chat_id=message.chat_id, photo=f)
 
-    # Кодируем текст в URL-подобную форму
-    data = {'text': text}
-    data = urllib.parse.urlencode(data).encode('ascii')
+# добавление обработчика команды /generate
+generate_handler = CommandHandler('generate', generate)
+dispatcher.add_handler(generate_handler)
 
-    # Отправляем POST-запрос на API нейросети
-    response = requests.post(api_url, data=data, headers=headers)
-
-    # Если ответ успешный, возвращаем URL изображения
-    if response.status_code == 200:
-        result = response.json()
-        return result['image_url']
-
-    # В противном случае возвращаем None
-    else:
-        return None
-
-# Функция main, которая запускает бота
-def main():
-    # Создание экземпляра бота и его токена
-    updater = Updater(token='6064933243:AAEgWPjI8DAhffvpKvIXVf8BDBzknzZ0sqI', use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Добавление обработчиков команд и сообщений
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-
-    text_handler = MessageHandler(Filters.text, text_message)
-    dispatcher.add_handler(text_handler)
-
-    # Запуск бота
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+# запуск бота
+updater.start_polling()
+updater.idle()
